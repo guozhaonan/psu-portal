8f203ebbdbfe5921bfa67b12b710b403d83f9917
Albert Guo <guozhaonan@gmail.com>
2017-08-15 21:41:01 -0400

    Add Users Signin Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :admins, controllers: {
     sessions:           "admins/sessions",
     passwords:          "admins/passwords",
@@ -25,9 +24,8 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   resources :posts
-  
+
   resources :conversations do
     resources :messages
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
